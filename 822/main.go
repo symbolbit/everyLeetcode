144477feb6aad041041184fee0486feefa06140d
糖醋溜肥肠 <2457784786@qq.com>
2024-08-22 08:30:48 +0800

    feat: 调整gitignore和readme

--- 822/main.go
+++ 822/main.go
@@ -32,12 +32,10 @@ import "fmt"
 */
 
 func minEnd(n, x int) int64 {
-	n-- // 先把 n 减一，这样下面讨论的 n 就是原来的 n-1
+	n--
 	i, j := 0, 0
 	for n>>j > 0 {
-		// x 的第 i 个比特值是 0，即「空位」
 		if x>>i&1 == 0 {
-			// 空位填入 n 的第 j 个比特值
 			x |= n >> j & 1 << i
 			j++
 		}
